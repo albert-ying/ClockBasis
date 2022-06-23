c5f6c9ea0b1ab138fc54756dd50f11e27179c97f
albert-ying <59846322+albert-ying@users.noreply.github.com>
2022-06-23 00:51:34 -0400

    Update

--- R/calc_DNAm_Age.R
+++ R/calc_DNAm_Age.R
@@ -5,56 +5,60 @@
 #' @importFrom dplyr arrange
 #-----------------------------------------------------------------------------
 
-clock_folder = "data/Clocks/Mouse/"
+{
+  require(readr)
+  require(purrr)
+  require(dplyr)
+  clock_folder = "data/Clocks/Mouse/"
 # [1] "Csaba.tsv"                   "MeerClock.tsv"               "PetkovichClock.tsv"          "ThompsonClock-all.tsv"      
 # [5] "ThompsonClock-conserved.tsv" "WangLiver.tsv" 
-meer = read_tsv(paste0(clock_folder, "MeerClock.tsv"))
-petkovich = read_tsv(paste0(clock_folder, "PetkovichClock.tsv"))
-thompson = read_tsv(paste0(clock_folder, "ThompsonClock-all.tsv"))
-wangliver = read_tsv(paste0(clock_folder, "WangLiver.tsv"))
-Meer_intercept = 234.64
-Petkovich_intercept = 0
-Thompson_intercept = 30.3172
-Wang_intercept = 5.827926399
+  meer = read_tsv(paste0(clock_folder, "MeerClock.tsv"))
+  petkovich = read_tsv(paste0(clock_folder, "PetkovichClock.tsv"))
+  thompson = read_tsv(paste0(clock_folder, "ThompsonClock-all.tsv"))
+  wangliver = read_tsv(paste0(clock_folder, "WangLiver.tsv"))
+  Meer_intercept = 234.64
+  Petkovich_intercept = 0
+  Thompson_intercept = 30.3172
+  Wang_intercept = 5.827926399
 
-mouse_clock.ls = list(meer, petkovich, thompson, wangliver)
+  mouse_clock.ls = list(meer, petkovich, thompson, wangliver)
 
 ## reformate the clock
-mouse_clock.ls = map(mouse_clock.ls, ~{
-  colnames(.x) = c("CpG", "Weight")
-  arrange(.x, CpG)
-})
+  mouse_clock.ls = map(mouse_clock.ls, ~{
+    colnames(.x) = c("CpG", "Weight")
+    arrange(.x, CpG)
+  })
 
-clock_intercept.ls = list(
-  Meer_intercept,
-  Petkovich_intercept,
-  Thompson_intercept,
-  Wang_intercept
-)
+  clock_intercept.ls = list(
+    Meer_intercept,
+    Petkovich_intercept,
+    Thompson_intercept,
+    Wang_intercept
+  )
 
-names(mouse_clock.ls) = c(
-  "MeerClock",
-  "PetkovichClock",
-  "ThompsonClock",
-  "WangLiver"
-)
+  names(mouse_clock.ls) = c(
+    "MeerClock",
+    "PetkovichClock",
+    "ThompsonClock",
+    "WangLiver"
+  )
 
-mouse_mean_me = read_csv("data/thompson_site_mean.csv")
+  mouse_mean_me = read_csv("data/thompson_site_mean.csv")
 
-mouse_imputer.ls = list()
-for (i in 1:length(mouse_clock.ls)) {
-  print(i)
-  clock = mouse_clock.ls[[i]]
-  mouse_imputer = clock |>
-    select(CpG) |>
-    arrange(CpG) |>
-    left_join(mean_me, by = "CpG") |>
-    pull(mean_level)
-  mouse_imputer[is.na(mouse_imputer)] = 0.5
-  mouse_imputer.ls[[i]] = mouse_imputer
-  names(mouse_imputer.ls)[i] = names(mouse_clock.ls)[i]
+  mouse_imputer.ls = list()
+  for (i in 1:length(mouse_clock.ls)) {
+    print(i)
+    clock = mouse_clock.ls[[i]]
+    mouse_imputer = clock |>
+      select(CpG) |>
+      arrange(CpG) |>
+      left_join(mouse_mean_me, by = "CpG") |>
+      pull(mean_level)
+    mouse_imputer[is.na(mouse_imputer)] = 0.5
+    mouse_imputer.ls[[i]] = mouse_imputer
+    names(mouse_imputer.ls)[i] = names(mouse_clock.ls)[i]
+  }
 }
-
 
 #-----------------------------------------------------------------------------
 #' Calculation function for mouse DNAm age
@@ -83,4 +87,7 @@ do_dnam_clock_mouse = function(data, ...) {
 # debug
 #-----------------------------------------------------------------------------
 if (FALSE) {
+  # remotes::install_github("albert-ying/ClockBasis") 
+  # library(ClockBasis)
+
 }
